26b80c1a76d905a74b791be43c9360e5d35c0cc3
vmateusvictor-alt <vmateus.victor@gmail.com>
2026-02-20 11:23:04 +0200

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -1,10 +1,7 @@
-from sources.mangaflix import MangaFlixSource
 from sources.toonbr import ToonBrSource
 
-_sources = {
-    "MangaFlix": MangaFlixSource(),
-    "ToonBr": ToonBrSource()
-}
 
 def get_all_sources():
-    return _sources
+    return {
+        "ToonBr": ToonBrSource(),
+    }
